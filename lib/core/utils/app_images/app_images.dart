020e48cf803be110e825f2acc6acd9894b8b157e
tazeen-dev <tazeenzahrabatool@gmail.com>
2025-08-13 12:07:31 -0700

    .

---
+++ lib/core/utils/app_images/app_images.dart
@@ -0,0 +1,24 @@
+class AppImages{
+  static const String carpenter='assets/images/carpenter.png';
+  static const String cleaning='assets/images/cleaning.png';
+  static const String companyImage='assets/images/companyimage.png';
+  static const String electricion='assets/images/electricion.png';
+  static const String more='assets/images/more.png';
+  static const String onboarding1='assets/images/onboarding1.png';
+  static const String onboarding2='assets/images/onboarding2.png';
+  static const String onboarding3='assets/images/onboarding3.png';
+  static const String profileEmplty='assets/images/profileempty.png';
+  static const String profilePhoto='assets/images/profilephoto.png';
+  static const String repairing='assets/images/repairing.png';
+  static const String splash='assets/images/splash.png';
+  static const String user1='assets/images/user1.png';
+  static const String user2='assets/images/user2.png';
+  static const String user3='assets/images/user3.png';
+  static const String waterResourse1='assets/images/water-resource.png';
+  static const String waterResourse2='assets/images/water-resource2.png';
+  static const String waterResourse3='assets/images/water-resource3.png';
+  static const String waterResourse4='assets/images/water-resource4.png';
+  static const String waterResourse5='assets/images/water-resource5.png';
+  static const String welcomeSplash ='assets/images/welcomscreen.png';
+  static const String workerMen='assets/images/workerMen.jpg';
+}
