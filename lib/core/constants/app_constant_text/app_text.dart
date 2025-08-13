a112f6dc69c823a6012b3ce5bddf116631dd6bd4
tazeen-dev <tazeenzahrabatool@gmail.com>
2025-08-13 12:43:48 -0700

    .

---
+++ lib/core/constants/app_constant_text/app_text.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class AppText extends StatelessWidget {
+  const AppText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppText(
+
+    );
+  }
+}
