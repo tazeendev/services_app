f2c2239ce7a535a437979a859e2c56e093f5332e
tazeen-dev <tazeenzahrabatool@gmail.com>
2025-08-13 06:52:27 -0700

    .

---
+++ lib/core/utils/app_colors/app_colors.dart
@@ -0,0 +1,24 @@
+import 'dart:ui';
+class AppColors{
+  static const Color blackColor=Color(0xff282828);
+  static const Color greenColor=Color(0xffCCFD04);
+  static const Color whiteColor=Color(0xffFFFFFF);
+  static const Color authColor=Color(0xffF6F6F6);
+  static const Color orangeColor=Color(0xffFF8800);
+  static const Color darkGreenColor=Color(0xff0F9918);
+  static const Color purpleColor=Color(0xff7A5AF8);
+  static const Color blueColor=Color(0xff3C88EE);
+  static const Color pinkColor=Color(0xffF16BC9);
+  static const Color meronColor=Color(0xffEA6A6A);
+  static const Color lightBlueColor=Color(0xff58A6FB);
+  static const Color DarkBlueColor=Color(0xff2F80ED);
+  static const Color skyColor=Color(0xffE2E8FF);
+  static const Color lightGreyColor=Color(0xffD9D9D9);
+  static const Color redColor=Color(0xffFC5151);
+  static const Color updateColor=Color(0xff18C8FF);
+  static const Color updatenumberColor=Color(0xff957BF9);
+  static const Color updateNameColor=Color(0xff3FAD46);
+  static const Color greyColor=Color(0xff737373);
+  static const Color feildTextColor=Color(0xff33353C);
+ // static const Color lightGreenColor=Color(0xff)
+}
