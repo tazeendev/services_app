e2a41ccc70ed8b21ee38a76afe58b88bd9dfe1be
tazeen-dev <tazeenzahrabatool@gmail.com>
2025-08-13 21:58:21 -0700

    .

--- lib/core/constants/app_constant_text/app_text.dart
+++ lib/core/constants/app_constant_text/app_text.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../utils/app_colors/app_colors.dart';
 class AppText extends StatelessWidget {
-  const AppText({super.key});
+  final String text;
+  final Color textColor;
+  final FontWeight fontWeight;
+  final TextAlign textAlign;
+  final double? customFontSize;
+  final String fontFamily;
+  AppText({super.key, required this.text, this.textColor=AppColors.blackColor, this.fontWeight=FontWeight.w600,
+    this.textAlign=TextAlign.center, this.customFontSize, this.fontFamily='Poopin',});
 
   @override
   Widget build(BuildContext context) {
-    return AppText(
-
+    return Text(
+      text,
+      style: GoogleFonts.getFont(fontFamily,fontSize: customFontSize,fontWeight: fontWeight,color: textColor, ),
+      textAlign: textAlign,
     );
   }
 }
