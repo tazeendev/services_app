8a9d535846d10fe9b2537563c3160343b4a94110
tazeen-dev <tazeenzahrabatool@gmail.com>
2025-08-17 03:30:51 -0700

    .

--- lib/controller/app_controllers/worker_controller/worker_register_controller.dart
+++ lib/controller/app_controllers/worker_controller/worker_register_controller.dart
@@ -10,11 +10,18 @@ import 'package:services_app/core/utils/app_images/app_images.dart';
 import '../../../../../app/app_linker/app_links.dart';
 class RegisterCompanyController extends GetxController {
   var selectWorkerType = 'Select Type'.obs;
+  var selectCountry="Select Country".obs;
   final List<Map<String, String>> workers = [
     {"title": "Plumber", "image": AppImages.repairing},
     {"title": "Cleaning", "image": AppImages.cleaning},
     {"title": "Carpenter", "image": AppImages.carpenter},
     {"title": "Electrician", "image": AppImages.electricion},
+  ];
+  final List<Map<String, String>> country = [
+    {"title": "Pakistan",},
+    {"title": "UAE", },
+    {"title": "Gulf",},
+    {"title": "India",},
   ];
   void setWorkerType(String type) {
     selectWorkerType.value = type;
@@ -71,5 +78,57 @@ class RegisterCompanyController extends GetxController {
         borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
       ),
     );
+    //---------------country bottomSheet-------------------
+    void showCountryBottomSheet() {
+      Get.bottomSheet(
+        Container(
+          padding: EdgeInsets.all(16),
+          child: Obx(
+                () => Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(height: 20),
+                AppText(text: 'Select Experience Country'),
+                SizedBox(height: 10),
+                ...workers.map((worker) {
+                  bool isSelected = selectWorkerType.value == worker['title'];
+                  return Column(
+                    children: [
+                      ListTile(
+                        leading: Image.asset(
+                          worker['image']!,
+                          width: 40,
+                          height: 40,
+                        ),
+                        title: Text(worker['title']!),
+                        trailing: Icon(
+                          isSelected
+                              ? Icons.check_circle
+                              : Icons.check_circle_outline,
+                          color: isSelected
+                              ? AppColors.blackColor
+                              : AppColors.greyColor,
+                        ),
+                        tileColor: isSelected
+                            ? AppColors.blackColor.withOpacity(0.2)
+                            : null,
+                        onTap: () {
+                          setWorkerType(worker['title']!);
+                        },
+                      ),
+                      Divider(),
+                    ],
+                  );
+                }).toList(),
+                CustomButton(text: 'Ok', onTap: (){}, height:30, width: double.infinity)
+              ],
+            ),
+          ),
+        ),
+        backgroundColor: AppColors.whiteColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+        ),
+      );
   }
 }
